5622603ba4d9b2929278e8d22fc0ec46829c472b
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2019-03-25 22:51:05 -0600

    home link to page header title and added page desc

--- header.php
+++ header.php
@@ -5,7 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php bloginfo('name');?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+  <?php wp_head(); ?>
 </head>
-<body>
-  
+<body <?php body_class();?>>
+  <header class="site-header">
+  <h1><a href="<?php echo home_url();?>"><?php bloginfo("name");?> </a></h1>
+  <h5><?php bloginfo("description") ?></h5>
+
+  </header>
